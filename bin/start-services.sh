1134cf4e11b308eff885e55b6a58aacf127f600a
Chris Ottinger <chris.ottinger@bigpond.com>
2018-04-15 23:10:10 +1000

    added a start / stop script

--- bin/start-services.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# start the workspace services
-docker-compose -f services/docker-compose.yml up -d
--- bin/stop-services.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# stop the workplace services
-docker-compose -f services/docker-compose.yml down
